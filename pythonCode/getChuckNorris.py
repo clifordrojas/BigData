2b1acf313938a2241f73af4e5ae7fb0c96e66591
Cliford Rojas <cliffordrojas@gmail.com>
2020-02-03 13:15:03 -0500

    Update getChuckNorris.py

--- pythonCode/getChuckNorris.py
+++ pythonCode/getChuckNorris.py
@@ -1,12 +1,24 @@
 import json
 import requests
 
+from io import StringIO
+
+
+output = StringIO()
+
+
 fileWriter = open("data.json", "w+")
 counter = 0
+stringIO = StringIO()
 while counter <= 10:
     response = requests.get("https://api.chucknorris.io/jokes/random")
-    fileWriter.write(str(response.json())+ "\n")
+    stringIO.write(str(response.json())+"\n")
+    print(str(response.json())+"\n")
     counter += 1
-    print("Working")
 
+
+data = stringIO.getvalue()
+
+fileWriter.write(data[0:len(data)])
+stringIO.close()
 fileWriter.close()
