87d6f37692877543730d84508472206c2624be11
Cliff Windows <Cliffordrojas@gmail.com>
2020-02-16 14:01:17 -0500

    Added Task 24

---
+++ Task-24/sparkStream.py
@@ -0,0 +1,54 @@
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+import requests
+from io import StringIO
+from hdfs import InsecureClient
+from pyspark.streaming.kafka import KafkaUtils
+from kafka import KafkaProducer
+
+
+def readAPI():
+    output = StringIO()
+    # fileWriter = open("data.json", "w+")
+    counter = 0
+    stringIO = StringIO()
+    while counter <= 2:
+        response = requests.get("https://api.chucknorris.io/jokes/random")
+        stringIO.write(str(response.json()) + "\n")
+        # print(str(response.json())+"\n")
+        counter += 1
+
+    data = stringIO.getvalue()
+    output.close()
+    return data
+def sendKafkaMessage(producer,lines):
+    producer.send("Chuck Norris", bytes(lines, 'utf-8'))
+    producer.flush()
+
+def handler(message):
+    records = message.collect()
+    for record in records:
+        producer.send('spark.out', str(record))
+        producer.flush()
+
+
+if __name__ == '__main__':
+    data = readAPI()
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+    #Send Kafka Message using Kafka Producer
+    sendKafkaMessage(producer,data)
+
+    #Spark Stream
+    sc = SparkContext.getOrCreate()
+    ssc = StreamingContext(sc, 10)
+
+    brokers, topic = sys.argv[1:]
+    stream = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
+    stream.foreachRDD(handler)
+
+    ssc.start()
+    ssc.awaitTermination()
+
+
+
