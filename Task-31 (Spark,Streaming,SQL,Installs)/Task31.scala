cb8db12b455c630f803f785fb4b9f6cf72f7c82e
Cliford Rojas <cliffordrojas@gmail.com>
2020-04-21 08:48:12 -0400

    4/21 Completed RDD Lineage

--- Task-31 (Spark,Streaming,SQL,Installs)/Task31.scala
+++ Task-31 (Spark,Streaming,SQL,Installs)/Task31.scala
@@ -24,7 +24,12 @@ import org.apache.spark
 //    import spark.implicits._
     val csv_rdd = sc.textFile("/home/desktop/Desktop/csv_file.csv")
     val csv_map = csv_rdd.flatMap(line => line.split(","))
-    val csv_line = csv_map.foreach(line => println(line.toString))
+    val csv_map2 = csv_map.map(line => (line(0), line(2)))
+    val csv_partition = csv_map2.repartition(200)
+    val csv_make_small = csv_partition.coalesce(4)
+    println(csv_make_small.toDebugString)
+
+
 
 //    val csv_file = spark.read.csv("/home/desktop/Desktop/csv_file.csv")
 //    val new_column = csv_file.withColumn("new_id",$"_c0"+1)
